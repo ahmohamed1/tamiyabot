742bc1723e8f3dd2c13bea886a52f86605b91488
ahmohamed1 <abdll1@hotmail.com>
2025-02-07 18:33:06 +0300

    fixed controller launch

--- tamiyabot_controller/launch/controller.launch.py
+++ tamiyabot_controller/launch/controller.launch.py
@@ -1,7 +1,9 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, GroupAction
 from launch_ros.actions import Node
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 
 
 def generate_launch_description():
@@ -11,35 +13,33 @@ def generate_launch_description():
         default_value="True",
     )
 
-    
     use_sim_time = LaunchConfiguration("use_sim_time")
 
-
-    joint_state_broadcaster_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=[
-            "joint_state_broadcaster",
-            "--controller-manager",
-            "/controller_manager",
-        ],
-    )
-
-    wheel_controller_spawner = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["tamiyabot_controller", 
-                   "--controller-manager", 
-                   "/controller_manager"
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare('tamiyabot_controller'),
+            'config',
+            'tamiyabot_controllers.yaml',
         ]
     )
 
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['joint_state_broadcaster'],
+    )
+    ackermann_steering_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['tamiyabot_controller', '--param-file', robot_controllers],
+        remappings=[('tamiyabot_controller/tf_odometry', 'tf')],
+    )
     
 
     return LaunchDescription(
         [
             use_sim_time_arg,
             joint_state_broadcaster_spawner,
-            wheel_controller_spawner,
+            ackermann_steering_controller_spawner,
         ]
     )
